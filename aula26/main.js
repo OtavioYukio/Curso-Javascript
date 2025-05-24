38df06eedc137f89461c2cde2083e67df768b8db
= <=>
2025-05-24 11:01:09 -0300

    Webpack

---
+++ aula26/main.js
@@ -0,0 +1,10 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    falar() {
+        console.log("oi")
+    }
+}
---
+++ aula27/public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var a = "a";
+var b = "b";
+console.log("".concat(a + b));
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
